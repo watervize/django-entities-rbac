47738309a262951b681bf6cc1b6ab6365161d6a5
Omen Apps <support@omenapps.com>
2022-09-04 16:20:32 -0400

    Remove obsolete tags and add skeletons for new

--- entities_rbac/templatetags/permission_tags.py
+++ entities_rbac/templatetags/permission_tags.py
@@ -6,29 +6,35 @@ from entities_rbac.checkers import has_role, has_permission, has_object_permissi
 register = template.Library()
 
 
-@register.filter(name="has_role")
-def has_role_template_tag(user, role):
-    role_list = role.split(",")
-    return has_role(user, role_list)
-
-
-@register.filter(name="can")
-def can_template_tag(user, role):
-    return has_permission(user, role)
-
-
-if hasattr(register, "assignment_tag"):
-    tag_registter = register.assignment_tag
-else:
-    tag_registter = register.simple_tag
-
-
-@tag_registter(name="can", takes_context=True)
-def has_permission_template_tag(context, permission, obj, user=None):
+@register.simple_tag(name="has", takes_context=True)
+def has_roles_template_tag(context, permission, obj, user=None):
     if not user:
         user = context.get("user")
 
     if user:
-        return has_object_permission(permission, user, obj)
+        return has_roles(permission, user, obj)
 
     return False
+
+
+@register.simple_tag(name="can", takes_context=True)
+def has_permissions_template_tag(context, permission, obj, user=None):
+    if not user:
+        user = context.get("user")
+
+    if user:
+        return has_object_permissions(permission, user, obj)
+
+    return False
+
+
+@register.simple_tag(name="list_roles", takes_context=True)
+def list_roles_template_tag(context, permission, obj, user=None):
+
+    return False  # ToDo: A list of roles
+
+
+@register.simple_tag(name="list_permissions", takes_context=True)
+def list_permissions_template_tag(context, permission, obj, user=None):
+
+    return False  # ToDo: A list of permissions
